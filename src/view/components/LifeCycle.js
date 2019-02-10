073ccafd40ce87929d4581122daba0b3899df7ef
Lucas Tostée <lucas.tostee@gmail.com>
2019-02-10 21:37:46 +0100

    Update LifeCycle component

--- src/view/components/LifeCycle.js
+++ src/view/components/LifeCycle.js
@@ -7,20 +7,30 @@ import React from "react";
  * Déclaration
  */
 class LifeCycle extends React.Component {
-    constructor(props) {
+    constructor(props) { //TODO: Called in first.
         super(props);
         console.log(`Called from the constructor function`);
-        this.state = {};
+        this.state = {
+            name: "Lucas"
+        };
     }
-    componentWillMount() {
+    componentWillMount() { // TODO: Called in second.
         console.log(`Called from the componentWillMount function`);
     }
-    componentDidMount() {
+    componentDidMount() { //TODO: Called in last.
         console.log(`Called from the componentDidMount function`);
     }
-    render() {
+    componentDidUpdate() { //TODO: Called last when update.
+        console.log(`Component was update`);
+    }
+    render() { //TODO: Called in third.
         console.log(`Called from the render function`);
-        return <h1>Hello internet</h1>
+        return <h1 onClick={this.setAlert}>Hello {this.state.name}</h1>
+    }
+    setAlert = () => {
+        this.setState({
+            name: "Tommy",
+        });
     }
 };
 
