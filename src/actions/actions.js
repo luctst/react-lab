2800a2121fd8bd563a61941cbcacb2de36474da4
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-02 01:06:46 +0200

    Add actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,5 @@
 /**
  * Import, variables..
  */
-export const ADD  = "ADD";
-export const DECREMENT = "DECREMENT";
-export const ADD_WITH = "ADD_WITH";
-export const DECREMENT_WITH = "DECREMENT_WITH";
+export const ADD_TASK = "ADD_TASK";
+export const DELETE = "DELETE";
