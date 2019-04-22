5dba303434599630b035ce5c955939d93760aa66
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-22 21:36:13 +0200

    Create MOVE_TASK_FUNCTION, UPDATE and DELETE

--- projects/todo/reducers/tasks/utility.js
+++ projects/todo/reducers/tasks/utility.js
@@ -9,25 +9,41 @@ export const ADD_TASK_FUNCTION = (state, action) => {
 		alert("This task already exist");
 		return state;
 	} else {
-		const newState = {...state};
+		const newState = { ...state };
 		newState.todo.push(action.value);
 		return newState;
 	}
 }
 
-export const DELETE_TASK_FUNCTION = (state, action) => {
-	const index = state[action.visibilityTab].indexOf(action.value);
-	const newState = {...state};
+export const MOVE_TASK_FUNCTION = (state, action) => {
+	if (action.data.visibilityTab === action.data.moveInTab) {
+		alert(`This task is already in the ${action.data.moveInTab} tab`);
+		return state;
+	} else if (state[action.data.moveInTab].includes(action.data.value)) {
+		alert(`${action.data.moveInTab} field already contains the "${action.data.value}" task.`);
+		return state;
+	} else {
+		const index = state[action.data.visibilityTab].indexOf(action.data.value);
+		const newState = { ...state };
 
-	newState[action.visibilityTab].splice(index, 1);
-	newState.deleted.push(action.value);
-	alert(`Task: ${action.value} move in deleted tab.`);
+		newState[action.data.visibilityTab].splice(index, 1);
+		newState[action.data.moveInTab].push(action.data.value);
 
-	return newState;
+		return newState;
+	}
 }
 
 export const UPTATE_TASK_FUNCTION = (state, action) => {
-	const newState = {...state};
+	const newState = { ...state };
 	newState[action.data.visibilityTab][action.data.indexToUpdate] = action.data.newValue;
+	return newState;
+}
+
+export const DELETE_TASK_FUNCTION = (state, action) => {
+	const newState = {...state};
+	const indexOfTask = newState.deleted.indexOf(action.data.taskToDelete);
+
+	newState.deleted.splice(indexOfTask, 1);
+
 	return newState;
 }
