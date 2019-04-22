0d29f3ead8f45da78ef1407f6def214f839779b0
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-22 21:29:28 +0200

    Add props

--- projects/todo/components/NoTask.js
+++ projects/todo/components/NoTask.js
@@ -3,16 +3,16 @@ import React from "react";
 /**
  * Render h1 title when there is no task
  */
-const NoTask = () => {
-    return (
-        <main className="container mt-3">
-            <div className="row">
-                <div className="col-12">
-                    <h1 className="text-center">No Tasks Here..</h1>
-                </div>
-            </div>
-        </main>
-    );
-}
+const NoTask = props => {
+	return (
+		<main className="container mt-3">
+			<div className="row">
+				<div className="col-12">
+					<h1 className="text-center">No Tasks in {props.tab} tab.</h1>
+				</div>
+			</div>
+		</main>
+	);
+};
 
 export default NoTask;
