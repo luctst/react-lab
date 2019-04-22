8e3874d82d33d3e228ca870c43f9dcb7552777c0
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-22 21:33:44 +0200

    add MOVE_TASK_FUNCTION

--- projects/todo/reducers/tasks/tasksReducer.js
+++ projects/todo/reducers/tasks/tasksReducer.js
@@ -1,15 +1,17 @@
 import appState from "../../state/state";
-import {ADD_TASK, DELETE, UPDATE} from "../../actions/tasks";
-import {ADD_TASK_FUNCTION, DELETE_TASK_FUNCTION, UPTATE_TASK_FUNCTION} from "./utility";
+import {ADD_TASK, DELETE, UPDATE, MOVE} from "../../actions/tasks";
+import {ADD_TASK_FUNCTION, DELETE_TASK_FUNCTION, UPTATE_TASK_FUNCTION, MOVE_TASK_FUNCTION} from "./utility";
 
 const tasksReducer = (state = appState.tasks, action) => {
     switch (action.type) {
         case ADD_TASK:
             return ADD_TASK_FUNCTION(state, action);
-		case DELETE:
-			return DELETE_TASK_FUNCTION(state, action);
+		case MOVE:
+			return MOVE_TASK_FUNCTION(state, action);
         case UPDATE:
 			return UPTATE_TASK_FUNCTION(state, action);
+		case DELETE:
+			return DELETE_TASK_FUNCTION(state, action);
         default:
             return state;
     }
