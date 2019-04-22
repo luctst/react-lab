0d7f22c100f821f4c244d3febf3eb3166fef0619
Lucas Tostée <lucas.tostee@gmail.com>
2019-04-22 02:26:29 +0200

    Use memo for Header component

--- projects/todo/components/Header.js
+++ projects/todo/components/Header.js
@@ -6,17 +6,17 @@ import React from "react";
 /**
  * Render the header with React.memo for performances.
  */
-const Header = () => {
+const Header = React.memo(() => {
     return (
-        <section className="container mt-3 mb-3">
+        <header className="container mt-3 mb-3">
             <div className="row">
                 <div className="col-12">
                     <h1>Todo app</h1>
                     <hr/>
                 </div>
             </div>
-        </section>
+        </header>
     );
-};
+})
 
-export default React.memo(Header);
+export default Header;
